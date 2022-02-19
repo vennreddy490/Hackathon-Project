06829b36eb6a990e8ee13b4376d480ded8987428
Daniel Grigsby <pastafrodo@gmail.com>
2022-02-18 22:36:29 -0500

    danielpush

--- src/Randomizer.java
+++ src/Randomizer.java
@@ -1,3 +1,14 @@
 public class Randomizer {
     
+    public Randomizer(int dimension) {
+
+    }
+
+    public char[] getKey() {
+        return char[];
+    }
+
+    public char[] getRevealed() {
+        return char[];
+    }
 }
