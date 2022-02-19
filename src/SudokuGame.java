1207ac56a3d8d31bb37fb808c64e33b927607572
danielgrigsby03 <daniel.grigsby.03@gmail.com>
2022-02-19 18:30:01 -0500

    SudokuGame includes parse commands and print board

--- src/SudokuGame.java
+++ src/SudokuGame.java
@@ -37,6 +37,41 @@ public class SudokuGame {
         {'7', '6', '3', '4', '1', '8', '2', '5', '9'}
     };
 
+    public void printBoard () {
+        //prints the contents already in the revealed array
+        for (int i = 0; i < revealed.length; i++) {
+            for (int j = 0; j < revealed[i].length; j++) {
+                //need to also figure out a way to print vertical lines after certain number of blocks based on dimensions of 2D Array
+                //should go after print out number
+                if((revealed.length == 9) && (i % 3 == 0)) {
+                    System.out.print (" | ");
+                }
+                
+                else if((revealed.length == 4) && (i % 2 == 0)) {
+                    System.out.print (" | ");
+                }
+                //if the revealed value is "_"
+                else if (revealed[i][j] == '_') {
+                    System.out.print ("[ " + revealed[i][j] + " ]");
+                }
+                else {
+                    System.out.print (" " + revealed[i][j] + " ");
+                }
+            }
+        }
+
+        for (int i = 0; i < dimensions; i++) {
+            for (int j = 0; j < dimensions; j++) {
+                //if statement: checks if a guess was already made for a square that has an underline and then prints the char passed in for the square 
+                if ((revealed[i][j] == '_') && (guessed[i][j] != '_'))
+                System.out.print (" " + guessed[i][j] + " ");
+                //else: basic grid square print out if no guess was made
+                if ((revealed[i][j] == '_') && (guessed[i][j] == '_'))
+                System.out.print (" _");
+            }
+        }
+    } //printBoard
+
     public void parseInput() {
         userInput = stdIn.next();
         switch (userInput) {
@@ -98,7 +133,7 @@ public class SudokuGame {
         public void cheat() {
             for (int i = 0; i < guessed.length; i++) {
                 for (int l = 0; l < guessed.length; l++) {
-                    guessed[i][l] = key[i][l];               }
+                    guessed[i][l] = key[i][l];               
             }
         }
         // runs the guess command
@@ -134,6 +169,14 @@ public class SudokuGame {
             System.err.println(nsee.getMessage());
             System.exit(2);
         }
+    }
+
+    public void play() {
+        
+    }
+
+    public void promptUser() {
+
     }
 
 
